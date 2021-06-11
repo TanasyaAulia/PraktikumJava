126d79b41279b47f62f279d8e565ba9785f8c7b6
Tanasya Aulia <Tanasyaaulia@gmail.com>
2021-06-11 19:46:01 +0800

    Input dengan Scanner

--- DataTypes.java
+++ DataTypes.java
@@ -1,7 +1,11 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main(String[]args){
+
+
+ Scanner scanner = new Scanner(System.in);
 
   String namaDepan = "Tanasya";
   String namaBelakang = "Aulia";
@@ -10,6 +14,21 @@ public class DataTypes{
   double ipk = 3.89764512;
   char nilaiAbjad = 'A';
   boolean cantik = true;
+
+  System.out.print ("Input Nama Depan : ");
+  namaDepan = scanner.nextLine();
+  System.out.print ("Input Nama Belakang : ");
+  namaBelakang = scanner.nextLine();
+  System.out.print ("Input Usia : ");
+  usia = scanner.nextInt();
+  System.out.print ("Input IPK : ");
+  ipk = scanner.nextDouble();
+  System.out.print ("");
+  System.out.print ("Input Nilai Abjad : ");
+  nilaiAbjad = scanner.next().charAt(0);
+  System.out.print ("Cantik? : ");
+  cantik = scanner.nextBoolean();
+
 
   System.out.println ("------------- OUTPUT -------------");
   System.out.println ("Nama Depan : " + namaDepan);
