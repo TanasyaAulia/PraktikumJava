1ace86091cf1685250b8ff7395792d6284692402
Tanasya Aulia <Tanasyaaulia@gmail.com>
2021-06-11 19:35:53 +0800

    Deklarasi Variabel dan Tipe Data

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String[]args){
+
+  String namaDepan = "Tanasya";
+  String namaBelakang = "Aulia";
+  int usia = 20;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean cantik = true;
+
+  }
+}
